7df23bc9ca9467f1d2ec269eb5da7b938338874f
darre <Darren.wjy1990@gmail.com>
2019-09-26 14:51:59 -0700

    add inputName and inputAbout less 40 chars condition

--- myScript.js
+++ myScript.js
@@ -10,31 +10,37 @@ function showAddArtistContent(){
 function add(){
     var inputName = document.getElementById("artistName").value;
     var inputAbout = document.getElementById("aboutArtist").value;
-    var inputURL = document.getElementById("imageUrl").value;
-    var divEachArtist = document.createElement('div');
-    divEachArtist.setAttribute('class','eachArtist');
-    var divEachArtistImg = document.createElement('div');
-    divEachArtistImg.setAttribute('class','eachArtistImg');
-    var img = document.createElement('img');
-    img.setAttribute('src',inputURL);
-    divEachArtistImg.appendChild(img);
-    divEachArtist.appendChild(divEachArtistImg);
-    var divEachArtistContent = document.createElement('div');
-    var artist_h5 = document.createElement('h5');
-    var artist_p = document.createElement('p');
-    artist_p.textContent = inputAbout;
-    artist_h5.textContent = inputName;
-    divEachArtistContent.setAttribute('class','eachArtistContent')
-    divEachArtistContent.appendChild(artist_h5);
-    divEachArtistContent.appendChild(artist_p);
-    divEachArtist.appendChild(divEachArtistContent);
-    var removeButton = document.createElement('button');
-    removeButton.textContent="Delete";
-    removeButton.setAttribute("class","removeButton");
-    removeButton.setAttribute("onclick","removeArtist(this);");
-    divEachArtistContent.appendChild(removeButton);
-    var ArtistList = document.getElementsByClassName("ArtistList");
-    ArtistList[0].appendChild(divEachArtist);
+    
+    if(inputName<=40&&inputAbout<=40){
+        var inputURL = document.getElementById("imageUrl").value;
+        var divEachArtist = document.createElement('div');
+        divEachArtist.setAttribute('class','eachArtist');
+        var divEachArtistImg = document.createElement('div');
+        divEachArtistImg.setAttribute('class','eachArtistImg');
+        var img = document.createElement('img');
+        img.setAttribute('src',inputURL);
+        divEachArtistImg.appendChild(img);
+        divEachArtist.appendChild(divEachArtistImg);
+        var divEachArtistContent = document.createElement('div');
+        var artist_h5 = document.createElement('h5');
+        var artist_p = document.createElement('p');
+        artist_p.textContent = inputAbout;
+        artist_h5.textContent = inputName;
+        divEachArtistContent.setAttribute('class','eachArtistContent')
+        divEachArtistContent.appendChild(artist_h5);
+        divEachArtistContent.appendChild(artist_p);
+        divEachArtist.appendChild(divEachArtistContent);
+        var removeButton = document.createElement('button');
+        removeButton.textContent="Delete";
+        removeButton.setAttribute("class","removeButton");
+        removeButton.setAttribute("onclick","removeArtist(this);");
+        divEachArtistContent.appendChild(removeButton);
+        var ArtistList = document.getElementsByClassName("ArtistList");
+        ArtistList[0].appendChild(divEachArtist);
+    }else{
+        alert("Need less 40 charactors for Artist Name and About");
+    }
+
 } 
 function removeArtist(btn){
     (((btn.parentNode).parentNode).parentNode).removeChild((btn.parentNode).parentNode);
